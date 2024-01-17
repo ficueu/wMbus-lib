a9a42d5d53a7c07f504d92c75ad739a815d06fbf
ficueu <strona@ficu.eu>
2024-01-17 20:43:33 +0100

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -69,6 +69,7 @@ bool decrypt_TPL_AES_CBC_IV(std::vector<unsigned char> &frame,
 
   // The content should be a multiple of 16 since we are using AES CBC mode.
   if (num_bytes_to_decrypt % 16 != 0) {
+    ESP_LOGVV(TAG, "Last part of frame has %d bytes.", num_bytes_to_decrypt % 16);
     assert (num_bytes_to_decrypt % 16 == 0);
   }
 
